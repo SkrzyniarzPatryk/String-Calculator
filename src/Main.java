985415e5a18d62bd0bc6a5632c846a478b7e940e
Patryk <patryk@skrz.int.pl>
2022-06-13 23:00:25 +0200

    Step 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        Add("/n,");
     }
     public static int Add(String numbers) {
         if (!numbers.isEmpty()) {
             String[] numbersArray = numbers.split(",");
             int result = 0;
             for (int i = 0; i < numbersArray.length; i++) {
-                result += Integer.parseInt(numbersArray[i]);
+                String[] numbersArrayNewLine = numbersArray[i].split("\n");
+                if(numbersArrayNewLine.length > 1) {
+                    for (String number : numbersArrayNewLine) {
+                        result += Integer.parseInt(number);
+                    }
+                } else {
+                    result += Integer.parseInt(numbersArray[i]);
+                }
             }
             return result;
         }
--- src/MainTest.java
+++ src/MainTest.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
     @Test
-    void add() {
+    void expectedResultTesting() {
         Assertions.assertEquals(0, Main.Add(""));
         Assertions.assertEquals(2, Main.Add("2"));
         Assertions.assertEquals(6, Main.Add("2,4"));
-        Assertions.assertEquals(35, Main.Add("2,4,10,19"));
-
+        Assertions.assertEquals(35, Main.Add("2,4,10\n19"));
     }
+
+    @Test
+    void expectedExceptionTesting() {
+        assertThrows(NumberFormatException.class, () -> Main.Add("3,\n"));
+    }
+
+
 }
